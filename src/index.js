5cd762a42fa30d8f62baf16cc5b39b76bd554366
ankitc-11 <geniusankit10.ac@gmail.com>
2021-05-28 19:38:03 +0530

    first commit

--- src/index.js
+++ src/index.js
@@ -1,17 +1,63 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from 'react'
+import ReactDOM from 'react-dom'
+import 'semantic-ui-css/semantic.min.css'
+import { Button, Form, Container, Header } from 'semantic-ui-react'
+import './App.css';
+import sheets from './api'
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+class App extends React.Component{
+    state = {
+        name: '',
+        age: '',
+        salary: '',
+        hobby: ''
+    }
+    changeHandler = (e) => {
+        this.setState({[e.target.name] : e.target.value})
+    }
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+    submitHandler = e => {
+        e.preventDefault();
+        sheets.post(this.state).then(response => {
+            this.setState({
+                name: '',
+                age: '',
+                salary: '',
+                hobby: ''
+            })
+            window.alert('Successfully submitted!')
+          })
+    }
+    render(){
+        return(
+            <Container fluid className="container">
+        <Header as='h2'>React Google Sheets!</Header>
+        <Form className="form" onSubmit={this.submitHandler}>
+          <Form.Field>
+            <label>Name</label>
+            <input placeholder='Enter your name' type="text" name = "name" value = {this.state.name} onChange={this.changeHandler}/>
+          </Form.Field>
+          <Form.Field>
+            <label>Age</label>
+            <input placeholder='Enter your age' type="number" name = "age" value = {this.state.age} onChange={this.changeHandler}/>
+          </Form.Field>
+          <Form.Field>
+            <label>Salary</label>
+            <input placeholder='Enter your salary' type="number" name = "salary" value = {this.state.salary} onChange={this.changeHandler}/>
+          </Form.Field>
+          <Form.Field>
+            <label>Hobby</label>
+            <input placeholder='Enter your hobby' type="text" name = "hobby" value = {this.state.hobby} onChange={this.changeHandler}/>
+          </Form.Field>
+          
+          <Button color="blue" type='submit'>Submit</Button>
+          </Form>
+      </Container>
+    )
+  }
+}
+
+ReactDOM.render(<App />,document.getElementById('root'))
+
+
+
